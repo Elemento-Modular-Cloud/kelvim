4261c802d5d9774e8b11434448b9cf96b34e3fb1
Gabriele Fronzé <sucre.91@hotmail.it>
2024-07-30 11:32:52 +0200

    Retrieving block devices

--- kelvim-autosnapshot.sh
+++ kelvim-autosnapshot.sh
@@ -6,5 +6,15 @@ readarray -t domain_array < <(sudo virsh list --all | awk 'NR>2 && $2 != "" {pri
 # Iterate over the array
 for domain in "${domain_array[@]}"; do
     echo "Processing domain: $domain"
-    # Add your processing logic here
+    
+    # Get block devices and load into an array
+    readarray -t blk_array < <(sudo virsh domblklist "$domain" | awk 'NR>2 && $1 != "" {print $1 " " $2}')
+    
+    # Iterate over the block devices array and print the name and source
+    for blk in "${blk_array[@]}"; do
+        # Split each line into target and source
+        target=$(echo $blk | awk '{print $1}')
+        source=$(echo $blk | awk '{print $2}')
+        echo "\tBlock device target: $target, source: $source"
+    done
 done
