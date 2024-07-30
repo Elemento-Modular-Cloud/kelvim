83e306f47b678c29ca0ed583572856927ff33f2a
Gabriele Fronzé <sucre.91@hotmail.it>
2024-07-30 12:53:51 +0200

    Adding colors

--- kelvim-autosnapshot.sh
+++ kelvim-autosnapshot.sh
@@ -4,6 +4,11 @@
 elimg_pattern=".*/[^/]+\.elimg(/|$)"
 img_pattern="^/tmp/elemento/exported/[0-9a-fA-F\-]{36}\.img$"
 
+# Colors
+color_blue='\033[94m'
+color_orange='\033[93m'
+color_end='\033[0m'
+
 # Load domain names into an array
 readarray -t domain_array < <(sudo virsh list --all | awk 'NR>2 && $2 != "" {print $2}')
 
@@ -25,22 +30,23 @@ for domain in "${domain_array[@]}"; do
         # Split each line into target and source
         target=$(echo $blk | awk '{print $1}')
         source=$(echo $blk | awk '{print $2}')
-        echo -e "\tBlock device target: $target, source: $source"
+        echo -e "$color_blue\tBlock device target: $target, source: $source $color_end"
 
         # Check if the source contains a folder ending with ".elimg"
         if [[ "$source" =~ $elimg_pattern ]]; then
             echo -e "\tSource is placed in a '.elimg'. Creating snapshots alongside."
             elimg_path=$(echo "$source" | sed -E 's|(/[^/]*\.elimg)/.*|\1|')
             echo -e "\t\tmkdir -p $elimg_path/snaps/$date_string"
-            echo -e "\t\tvirtnbdbackup -d $domain -l auto -o $elimg_path/snaps/$date_string -i $target"
+            echo -e "\t\tvirtnbdbackup -d $domain -i $target -l auto -o $elimg_path/snaps/$date_string"
             # Operation A (e.g., creating a snapshot, logging, etc.)
         elif [[ "$source" =~ $img_pattern ]]; then
             echo -e "\tSource locally mounted via storageserver export. Creating snapshots on external backup media $ext_backup_media."
             uuid=$(echo "$source" | awk -F'/' '{print $NF}' | awk -F'.img' '{print $1}')
             echo -e "\t\tmkdir -p $ext_backup_media/$uuid.elsnaps/$date_string"
+            echo -e "\t\tvirtnbdbackup -d $domain -i $target -l auto -o $ext_backup_media/$uuid.elsnaps/$date_string"
             # Operation B (e.g., copying files, notifying, etc.)
         else
-            echo "Cannot handle this volume since it's not Elemento-based"
+            echo "\t\tCannot handle this volume since it's not Elemento-based"
         fi
     done
 done
