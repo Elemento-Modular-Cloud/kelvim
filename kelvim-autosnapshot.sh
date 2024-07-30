cc7c10048a388634ec6c9ba1403864ee72980f7f
Gabriele Fronzé <sucre.91@hotmail.it>
2024-07-30 12:29:46 +0200

    Detecting elimg

--- kelvim-autosnapshot.sh
+++ kelvim-autosnapshot.sh
@@ -3,6 +3,12 @@
 # Load domain names into an array
 readarray -t domain_array < <(sudo virsh list --all | awk 'NR>2 && $2 != "" {print $2}')
 
+# Date string used to create the right folder
+date_string=$(date +"%y%m%d")
+
+# External backup media
+ext_backup_media="/mnt/elemento-vault/snaps"
+
 # Iterate over the array
 for domain in "${domain_array[@]}"; do
     echo "Processing domain: $domain"
@@ -16,5 +22,14 @@ for domain in "${domain_array[@]}"; do
         target=$(echo $blk | awk '{print $1}')
         source=$(echo $blk | awk '{print $2}')
         echo -e "\tBlock device target: $target, source: $source"
+
+        # Check if the source contains a folder ending with ".elimg"
+        if [[ "$source" =~ .*/[^/]+\.elimg(/|$) ]]; then
+            echo -e "\tSource is placed in a '.elimg'. Creating snapshots alongside."
+            # Operation A (e.g., creating a snapshot, logging, etc.)
+        else
+            echo -e "\tSource locally mounted. Creating snapshots on external backup media $ext_backup_media."
+            # Operation B (e.g., copying files, notifying, etc.)
+        fi
     done
 done
