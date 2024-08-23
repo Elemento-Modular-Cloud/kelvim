b39eb6297e52df5031595d61df4e744ae69a20e2
Gabriele Fronzé <sucre.91@hotmail.it>
2024-08-23 19:24:06 +0200

    Adding tabulations

--- kelvim-snapshot-lister.sh
+++ kelvim-snapshot-lister.sh
@@ -22,7 +22,7 @@ color_green='\033[92m'
 color_end='\033[0m'
 
 # Variables
-if [ -z $backup_source ]; then
+if [ -z "$backup_source" ]; then
     backup_source=$(pwd)
 fi
 
@@ -31,31 +31,28 @@ echo -e "${color_purple}\nStarting Elemento Kelvim Lister utility ($(date +"%Y-%
 checkpoints_path="$backup_source/checkpoints"
 snapshots=($(ls -1 $checkpoints_path | sort -t '.' -k 2,2n))
 
+# Print table headers
+printf "%-20s %-10s %-10s %-20s\n" "Snapshot" "Size" "Chksum" "Date"
+
 # Iterate over the array and print each element without the .xml extension
 for snapshot in "${snapshots[@]}"; do
-    echo -e "\nHandling snapshot ${snapshot}"
-
     # Remove the .xml extension to match the data files
     base_name="${snapshot%.xml}"
 
     # Handle first snapshot differently since it's a full backup
-    if [ $base_name == "virtnbdbackup.0" ]; then
+    if [ "$base_name" == "virtnbdbackup.0" ]; then
         data_file="*.full.data"
     else
         data_file="*.$base_name.data"
     fi
-    
+
     # Use du to get the size of files matching the pattern
-    size_cmd="du -h $backup_source/$data_file | awk '{print \$1}'"
-    size=$(eval "$size_cmd")
+    size=$(du -h "$backup_source/$data_file" | awk '{print $1}')
+
+    chksum=$(cat "$backup_source/$data_file.chksum")
     
-    chksum_cmd="cat $backup_source/$data_file.chksum"
-    chksum=$(eval "$chksum_cmd")
+    date=$(stat -c %y "$backup_source/$data_file")
 
-    date_cmd="stat -c %y $backup_source/$data_file"
-    date=$(eval "$date_cmd")
-
-    echo "Size: $size"
-    echo "Chksum: $chksum"
-    echo "Date: $date"
+    # Print table row
+    printf "%-20s %-10s %-10s %-20s\n" "$snapshot" "$size" "$chksum" "$date"
 done
