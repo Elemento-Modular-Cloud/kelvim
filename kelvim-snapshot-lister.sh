8d4e4d6fde36499fca46d9ad2a5c1960e78f8678
Gabriele Fronzé <sucre.91@hotmail.it>
2024-08-23 18:42:50 +0200

    Adding output

--- kelvim-snapshot-lister.sh
+++ kelvim-snapshot-lister.sh
@@ -26,18 +26,20 @@ if [ -z $backup_source ]; then
     backup_source=$(pwd)
 fi
 
+echo -e "${color_purple}\nStarting Elemento Kelvim Lister utility ($(date +"%Y-%m-%d %H:%M:%S"))${color_end}"
+
 checkpoints_path="$backup_source/checkpoints"
 snapshots=$(ls -1 $checkpoints_path | sort -t '.' -k 2,2n)
 
 # Iterate over the array and print each element without the .xml extension
 for snapshot in "${snapshots[@]}"; do
-    echo "${snapshot}"
+    echo "Handling snapshot ${snapshot}"
     
     # Remove the .xml extension to match the data files
     base_name="${snapshot%.xml}"
     
     # Use du to get the size of files matching the pattern
-    size=$(du "$backup_source/${base_name}.data" 2>/dev/null)
+    size=$(du "$backup_source/${base_name}.data")
 
-    echo "$size"
+    echo "Size: $size"
 done
