0439f7f44cd8b75782723fd92883403583e046fc
Gabriele Fronzé <sucre.91@hotmail.it>
2024-08-23 19:19:43 +0200

    Debugging first snapshot

--- kelvim-snapshot-lister.sh
+++ kelvim-snapshot-lister.sh
@@ -37,12 +37,15 @@ for snapshot in "${snapshots[@]}"; do
 
     # Remove the .xml extension to match the data files
     base_name="${snapshot%.xml}"
+    echo "$base_name"
 
-    if [ $base_name == "*virtnbdbackup.0*" ]; then
+    if [ $base_name == "virtnbdbackup.0" ]; then
         data_file="*.full.data"
     else
         data_file="*.$base_name.data"
     fi
+
+    echo "$data_file"
     
     # Use du to get the size of files matching the pattern
     size_cmd="du -h $backup_source/$data_file | awk '{print \$1}'"
