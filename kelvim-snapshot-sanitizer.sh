6c05c03384908a4bd5781608b946164b8a3fcfdd
Gabriele Fronzé <sucre.91@hotmail.it>
2024-08-29 11:31:31 +0200

    Introducing sanitizer

---
+++ kelvim-snapshot-sanitizer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        -d|--domain)
+            input_domain="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+# Colors
+color_blue='\033[94m'
+color_red='\033[91m'
+color_orange='\033[93m'
+color_purple='\033[95m'
+color_green='\033[92m'
+color_end='\033[0m'
+
+checkpoints=($(sudo virsh checkpoint-list $input_domain | awk 'NR>2 {print $1}'))
+
+echo $checkpoints
