2af89dd4603993f0f7eeacfd1dd50aada7b10d8f
Gabriele Fronzé <sucre.91@hotmail.it>
2024-09-05 17:02:54 +0200

    Using another filler

--- kelvim-img-convert.sh
+++ kelvim-img-convert.sh
@@ -63,8 +63,8 @@ progress_bar() {
     local filled_length=$(( (progress * bar_length) / 100 ))
 
     # Create the filled portion (using #) and unfilled portion (using -)
-    local bar=$(printf "%-${filled_length}s" "#" | tr ' ' '#')
-    local empty=$(printf "%-$((bar_length - filled_length))s" "-")
+    local bar=$(printf "%-${filled_length}s" "-" | tr ' ' '-')
+    local empty=$(printf "%-$((bar_length - filled_length))s" ">")
 
     # Print the progress bar on the same line
     printf "\rProgress: [%s%s] %d%%" "$bar" "$empty" "$progress"
