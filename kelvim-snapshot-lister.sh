e9d466924a01c38ba0ab03944261bb4c595f7f29
G1org1owo <77230371+G1org1owo@users.noreply.github.com>
2025-04-02 18:15:26 +0200

    Redirect error messages to stderr and fix img-convert not failing when appropriate

--- kelvim-snapshot-lister.sh
+++ kelvim-snapshot-lister.sh
@@ -7,7 +7,7 @@ while [[ "$#" -gt 0 ]]; do
             shift 2
             ;;
         *)
-            echo "Unknown option: $1"
+            echo "Unknown option: $1" >&2
             exit 1
             ;;
     esac
